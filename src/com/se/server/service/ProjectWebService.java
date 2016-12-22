0f1325365eae5124b2923980b19f5dffaa514423
simon66eerrp <simon66eerrp66@gmail.com>
2016-12-22 18:40:53 +0800

    service_add_api

---
+++ src/com/se/server/service/ProjectWebService.java
@@ -0,0 +1,66 @@
+package com.se.server.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.se.server.repository.IssueGroupRepository;
+import com.se.server.repository.IssueRepository;
+import com.se.server.repository.MemberGroupRepository;
+import com.se.server.repository.ProjectRepository;
+import com.se.server.repository.UserRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+@Transactional("jpaTransactionManager")
+public class ProjectWebService {
+	@Autowired
+	IssueGroupRepository issueGroupRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	ProjectRepository projectRepository;
+	@Autowired
+	IssueRepository issueRepository;
+	@Autowired
+	MemberGroupRepository memberGroupRepository;
+	
+	@RequestMapping(value = "/projects/{userId}", method = RequestMethod.POST)
+	public void createProject(@PathVariable int userId,@RequestBody int a){
+		
+	}
+	
+	@RequestMapping(value = "/projects/{userId}/{projectId}", method = RequestMethod.GET)
+	public void getProjectInfo(@PathVariable int userId,@RequestBody int a){
+		
+	}
+	
+	@RequestMapping(value = "/projects/list/{userId}", method = RequestMethod.GET)
+	public void getProjectListByUserId(@PathVariable int userId){
+		
+	}
+	
+	@RequestMapping(value = "/projects/{userId}", method = RequestMethod.GET)
+	public void getInvitedProjectListByUserId(@PathVariable int userId){
+		
+	}
+	
+	@RequestMapping(value = "/all-projects/{userId}", method = RequestMethod.GET)
+	public void getAllProjectList(@PathVariable int userId){
+		
+	}
+	
+	@RequestMapping(value = "/projects/{userId}/{projectId}", method = RequestMethod.PUT)
+	public void updateProjectInfo(@PathVariable int userId,@PathVariable int projectId,@RequestBody int a){
+		
+	}
+	
+	@RequestMapping(value = "/projects/{userId}/{projectId}", method = RequestMethod.DELETE)
+	public void deleteProject(@PathVariable int userId,@PathVariable int projectId){
+		
+	}
+}
--- src/com/se/server/service/UserService.java
+++ src/com/se/server/service/UserService.java
@@ -2,6 +2,7 @@ package com.se.server.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,6 +36,36 @@ public class UserService {
 		user.setRole("role");
 		userRepository.save(user);
 		return 0;
+	}
+	
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	public void createUser(@RequestBody int a){
+		
+	}
+	
+	@RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
+	public void getUserInfo(@PathVariable int userId){
+		
+	}
+	
+	@RequestMapping(value = "/users/List/{userId}", method = RequestMethod.GET)
+	public void getUserList(@PathVariable int userId){
+		
+	}
+	
+	@RequestMapping(value = "/users/{userId}", method = RequestMethod.PUT)
+	public void updateUserInfo(@PathVariable int userId,@RequestBody int a){
+		
+	}
+	
+	@RequestMapping(value = "/users/{userId}/{delUserId}", method = RequestMethod.DELETE)
+	public void deleteUserInfo(@PathVariable int userId,@PathVariable int delUserId){
+		
+	}
+	
+	@RequestMapping(value = "/session", method = RequestMethod.POST)
+	public void login(@RequestBody int a){
+		
 	}
 	
 
