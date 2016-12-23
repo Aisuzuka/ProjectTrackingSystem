ea0430bb2a14c9b9f929ce0bb6502aa1cfe6064a
AiSuzuka <t102360053@ntut.org.tw>
2016-12-23 12:55:32 +0800

    MemberService, 未完成

--- src/com/se/server/entity/MemberGroup.java
+++ src/com/se/server/entity/MemberGroup.java
@@ -1,17 +1,13 @@
 package com.se.server.entity;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -44,5 +40,8 @@ public class MemberGroup {
 	private boolean isJoined;
 	
 
+	public boolean getJoined(){
+		return isJoined;
+	}
 
 }
--- src/com/se/server/repository/MemberGroupRepository.java
+++ src/com/se/server/repository/MemberGroupRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.se.server.entity.*;
 
 public interface MemberGroupRepository extends CrudRepository<MemberGroup, Integer>{
-
+	
+	public MemberGroup findByProjectId(int projectId);
+	
+	public MemberGroup findByUserId(int userId);
 }
